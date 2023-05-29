870cb9ff83ec7c076f53b4a6a6653cf065f395a2
João Luzio <joao.luzio@gmail.com>
2023-05-29 20:06:22 +0100

    Examples

---
+++ keycloak-playground/providers/src/test/java/com/example/keycloak/providers/KeycloakExampleIT.java
@@ -0,0 +1,27 @@
+package com.example.keycloak.providers;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.keycloak.admin.client.Keycloak;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@Slf4j
+class KeycloakExampleIT {
+
+  @Container
+  private KeycloakContainer container = new KeycloakContainer("quay.io/keycloak/keycloak:21.1")
+      .withRealmImportFile("/keycloak/app-realm-export.json");
+
+//  @Test
+  void test() {
+    // TODO: fix test, currently does not work
+    Keycloak keycloakAdmin = container.getKeycloakAdminClient();
+    keycloakAdmin.realms().findAll().forEach(realm -> {
+      log.info("realm: {}", realm.getRealm());
+    });
+  }
+
+}
